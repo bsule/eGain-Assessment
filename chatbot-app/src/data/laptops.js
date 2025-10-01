11055e872e7af3b72ae306fc2cf309b1a96bcc99
bsule <bilal.suleiman@gmail.com>
2025-10-01 16:41:24 -0700

    Adding laptops for chatbot

---
+++ chatbot-app/src/data/laptops.js
@@ -0,0 +1,74 @@
+export const laptops = [
+  {
+    id: 'budget-13',
+    name: 'Dell Inspiron 13 3000',
+    price: '$399',
+    specs: 'Intel Core i3, 8GB RAM, 256GB SSD, 13.3"',
+    bestFor: 'Basic tasks, web browsing, office work',
+    screenSize: '13'
+  },
+  {
+    id: 'budget-15',
+    name: 'Dell Inspiron 15 3000',
+    price: '$399',
+    specs: 'Intel Core i3, 8GB RAM, 256GB SSD, 15.6"',
+    bestFor: 'Basic tasks, web browsing, office work',
+    screenSize: '15'
+  },
+  {
+    id: 'mid-range-13',
+    name: 'HP Pavilion 13',
+    price: '$699',
+    specs: 'Intel Core i5, 16GB RAM, 512GB SSD, 13.3"',
+    bestFor: 'Multitasking, light gaming, content creation',
+    screenSize: '13'
+  },
+  {
+    id: 'mid-range-15',
+    name: 'HP Pavilion 15',
+    price: '$699',
+    specs: 'Intel Core i5, 16GB RAM, 512GB SSD, 15.6"',
+    bestFor: 'Multitasking, light gaming, content creation',
+    screenSize: '15'
+  },
+  {
+    id: 'gaming-15',
+    name: 'ASUS ROG Strix G15',
+    price: '$1,299',
+    specs: 'AMD Ryzen 7, 16GB RAM, 1TB SSD, RTX 4060, 15.6"',
+    bestFor: 'Gaming, video editing, 3D modeling',
+    screenSize: '15'
+  },
+  {
+    id: 'gaming-17',
+    name: 'ASUS ROG Strix G17',
+    price: '$1,399',
+    specs: 'AMD Ryzen 7, 16GB RAM, 1TB SSD, RTX 4060, 17.3"',
+    bestFor: 'Gaming, video editing, 3D modeling',
+    screenSize: '17'
+  },
+  {
+    id: 'premium-13',
+    name: 'MacBook Air 13"',
+    price: '$1,199',
+    specs: 'Apple M3, 16GB RAM, 512GB SSD, 13.6"',
+    bestFor: 'Professional work, creative projects, development',
+    screenSize: '13'
+  },
+  {
+    id: 'premium-14',
+    name: 'MacBook Pro 14"',
+    price: '$1,999',
+    specs: 'Apple M3, 16GB RAM, 512GB SSD, 14.2"',
+    bestFor: 'Professional work, creative projects, development',
+    screenSize: '14'
+  },
+  {
+    id: 'premium-16',
+    name: 'MacBook Pro 16"',
+    price: '$2,499',
+    specs: 'Apple M3, 16GB RAM, 512GB SSD, 16.2"',
+    bestFor: 'Professional work, creative projects, development',
+    screenSize: '16'
+  }
+];
